288d7d20290f00e31221791fc57576ee856cd69b
tkivela <kiveltom@gmail.com>
2021-01-20 14:54:45 +0200

    Update fish_prompt.fish
    Change __fish_git_prompt_showstashstate to false

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -25,14 +25,13 @@ function fish_prompt
  
   # Configure __fish_git_prompt
   set -g __fish_git_prompt_char_stateseparator ' '
-  set -g __fish_git_prompt_char_stashstate ''
   set -g __fish_git_prompt_color 5fdfff
   set -g __fish_git_prompt_color_flags df5f00
   set -g __fish_git_prompt_color_prefix white
   set -g __fish_git_prompt_color_suffix white
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_showuntrackedfiles true
-  set -g __fish_git_prompt_showstashstate true
+  set -g __fish_git_prompt_showstashstate false
   set -g __fish_git_prompt_show_informative_status true 
  
   # Line 1
