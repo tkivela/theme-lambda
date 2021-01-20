12d0d0f0a136515740e3555211cf12ae0aef2987
tkivela <kiveltom@gmail.com>
2021-01-20 15:25:43 +0200

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -31,8 +31,7 @@ function fish_prompt
   set -g __fish_git_prompt_color_suffix white
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_showuntrackedfiles true
-  # set -g __fish_git_prompt_showstashstate false
-  # set -g __fish_git_prompt_show_informative_status
+  set -g __fish_git_prompt_show_informative_status
  
   # Line 1
   echo -n $white$__fish_prompt_char' '$limegreen(pwd)$blue
